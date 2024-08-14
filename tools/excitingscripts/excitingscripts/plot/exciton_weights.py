36f9d6e723f5d9e247cbb0ea62594bc51f714609
Chung <dev.chpxu@outlook.com>
2024-08-14 10:54:30 +0200

    Subplots for exciton weight plotting.

--- tools/excitingscripts/excitingscripts/plot/exciton_weights.py
+++ tools/excitingscripts/excitingscripts/plot/exciton_weights.py
@@ -32,7 +32,7 @@ def option_parser():
         grid
         nrows
         ncols
-
+        path
     :return input_options: Dictionary of parsed command line arguments
     """
     p = ap.ArgumentParser(description=\
@@ -47,7 +47,7 @@ def option_parser():
 
     help_grid = 'If present, a grid is plotted in correspondence to the position of the major ticks.'
 
-    help_scale = "One float corresponding to the scaling factor in the horizontal and vertical size of the plot appearence."
+    help_scale = "Array of float corresponding to the scaling factor in the horizontal and vertical size of the exciton circles for each plot."
 
     help_ymin = "Minimum y-value to output from."
     help_ymax = "Maximum y-value to output from."
@@ -56,6 +56,7 @@ def option_parser():
     help_ncols = "Number of columns in subplot."
     help_sharex = "Whether the x-axes in subplot should share the same domain."
     help_sharey = "Whether the y-axes in subplot should share the same domain."
+    help_path = "Path through Brillouin Zone to print on the x-axis of bandstructure. Default is 'hexagonal' which corresponds to the tuple ('W','L','$\Gamma$','X','W','K')"
     #---------------------------------------------------------------------------
 
     p.add_argument('-d','--directory',
@@ -66,12 +67,10 @@ def option_parser():
                    nargs = '*', default = [],
                    type = str, help = help_files)
 
-    p.add_argument('-y1','--ymin',
-                   nargs = '*', default = [None, None],
+    p.add_argument('-y1','--ymin', default = [None, None],
                    type = float, help = help_ymin)
 
-    p.add_argument('-y2','--ymax',
-                   nargs = '*', default = [None, None],
+    p.add_argument('-y2','--ymax', default = [None, None],
                    type = float, help = help_ymax)
 
     p.add_argument('-s','--scale', default = 1.0,
@@ -84,7 +83,7 @@ def option_parser():
 
     p.add_argument('-nr','--nrows', type=int, help = help_nrows)
     p.add_argument('-nc','--ncols', type=int,  help = help_ncols)
-
+    p.add_argument('-p', '--path', type=str, help=help_path, default='hexagonal',choices=['hexagonal', 'cubic'])
 
     # p.add_argument('-lp','--legend_position',
     #                type = str, help = help_legend_position,
@@ -95,8 +94,8 @@ def option_parser():
     # p.add_argument('-nl','--no_legend', action='store_true', help = help_no_legend)
 
     p.add_argument('-g','--grid', action='store_true', help = help_grid)
-    p.add_argument('-shx','--share_x', default=True, help = help_sharex)
-    p.add_argument('-shy','--share_y', default=True, help = help_sharey)
+    p.add_argument('-shx','--share_x', default="col", help = help_sharex)
+    p.add_argument('-shy','--share_y', default="col", help = help_sharey)
 
 
     #---------------------------------------------------------------------------
@@ -111,10 +110,10 @@ def option_parser():
                  +"        PLOT-exciton-weights.py -f [FILES [FILES ...]]\n")
     input_options['files'] = args.files
 
-    input_options['ymin'] = None
-    if ( len(args.yboundary) >= 1 ): input_options['ymin'] = args.yboundary[0]
-    input_options['ymax'] = None
-    if ( len(args.yboundary) >= 2 ): input_options['ymax'] = args.yboundary[1]
+    input_options['ymin'] = args.ymin
+    #if ( len(args.yboundary) >= 1 ): input_options['ymin'] = args.yboundary[0]
+    input_options['ymax'] = args.ymax
+    #if ( len(args.yboundary) >= 2 ): input_options['ymax'] = args.yboundary[1]
 
     input_options['scale'] = args.scale
     input_options['title'] = args.title
@@ -124,47 +123,13 @@ def option_parser():
     input_options['ncols'] = args.ncols
     input_options['share_x'] = args.share_x
     input_options['share_y'] = args.share_y
+    input_options['path'] = args.path
 
     return input_options
 
-
-
-
-# narg = len(sys.argv)-1
-# if( narg < 1):
-#     print( "\n** ERROR: Must specify name of file and direction on command line.\n")
-#     print( "** Usage: ", sys.argv[0], " <KS band structure width weights>")
-#     sys.exit(0)
-# if( not os.path.isfile( sys.argv[1])):
-#     print( "\n** ERROR: Input file %s was not found." % sys.argv[1])
-#     sys.exit(0)
-# inname = sys.argv[1].strip()
-# if( narg == 1):
-#     scale = 1.0
-# elif( narg == 2):
-#     scale = float( sys.argv[2])
-# elif( narg == 3):
-#     ymin = float( sys.argv[2])
-#     ymax = float( sys.argv[3])
-#     scale = 1.0
-# else:
-#     ymin = float( sys.argv[2])
-#     ymax = float( sys.argv[3])
-#     scale = float( sys.argv[4])
-# scale *= 2e2
-# tmp = inname.split( '.')
-# tmp[-1] = 'png'
-# outname1 = inname+'.png'
-# tmp[-1] = 'pdf'
-# outname2 = inname+'.pdf'
-# tmp[-1] = 'eps'
-# outname3 = inname+'.eps'
-# tmp[-1] = 'svg'
-# outname4 = inname+'.svg'
-
-# Remove or add image formats
-output_formats: list = ["png", "eps", "pdf", "svg"]
-def read_input(filename: str) -> tuple:
+# Remove or add image output formats
+output_formats: list = ["png", "eps"]
+def read_input(filename: str):
         """
         Reads KPATH and BANDLINE data.
         """
@@ -172,6 +137,15 @@ def read_input(filename: str) -> tuple:
         bandlin, bandlindim = readrawdata( '../BANDLINES.OUT')
         bandlines = bandlin[:,0,0]
         return bandwgt, bandwgtdim, bandlin, bandlindim, bandlines
+def l2norm(x,y):
+    abs2 = np.multiply(y,y)
+    #print(len(abs2))
+    #print(len(x))
+    integral = np.trapz(abs2,x)
+    if integral == 0:
+        return y
+    else:
+        return y / np.sqrt(integral)
 
 def main(input_options):
     '''
@@ -191,8 +165,9 @@ def main(input_options):
     nrows: int = input_options['nrows']
     share_x: bool = input_options['share_x']
     share_y: bool = input_options['share_y']
+    path: tuple = input_options['path']
     
-
+    path_BZ = ('A','H','$\Gamma$','M','W','K') if path == 'hexagonal' else ('W','L','$\Gamma$','X','W','K')
     if (len(files) > nrows * ncols):
         print("WARNING: Number of plots greater than available subplot space. Please increase either nrows or ncols.")
         sys.exit(1)
@@ -206,103 +181,78 @@ def main(input_options):
     fig.patch.set_edgecolor(figcolor)
     fig.patch.set_facecolor(figcolor)
 
-    mpl.rcParams['axes.linewidth'] = 3.0 # set the value globally
-    mpl.rcParams['grid.linewidth'] = 1.5
-    mpl.rcParams['xtick.labelsize'] = 30
-    mpl.rcParams['ytick.labelsize'] = 30
+    mpl.rcParams['axes.linewidth'] = 1.0 # set the value globally
+    mpl.rcParams['grid.linewidth'] = 1.0
+    mpl.rcParams['xtick.labelsize'] = 72
+    mpl.rcParams['ytick.labelsize'] = 72
     mpl.rcParams['axes.edgecolor'] = 'black'
-    mpl.rcParams['axes.labelsize'] = '30'     # fontsize of the x any y labels
+    mpl.rcParams['axes.labelsize'] = '108'     # fontsize of the x and y labels
     mpl.rcParams['axes.labelcolor'] = 'black'
     mpl.rcParams['axes.axisbelow'] = 'True'   # whether axis gridlines and ticks are below
                                             # the axes elements (lines, text, etc)
     mpl.rcParams['legend.fontsize'] = 25
-    plt.rcParams['xtick.major.pad'] = 10
-    plt.rcParams['ytick.major.pad'] = 10
+    plt.rcParams['xtick.major.pad'] = 5
+    plt.rcParams['ytick.major.pad'] = 5
 
     plt.rcParams.update({'mathtext.default':'regular'})
 
     # Extract Data and Plot
     data = []
-    num_plots = axs.flatten()
+   # num_plots = axs.flatten()
     counter = 0
+    print(files)
     for row in axs:
+        row[0].set_ylabel( 'Energy [eV]')
         for col in row:
             col2 = col.twinx()
             output = read_input(files[counter])
-            col.xaxis.grid( True, which='major', color='k', linestyle='-', linewidth=2)
+            col.xaxis.grid( True, which='major', color='k', linestyle='-', linewidth=1)
             col.xaxis.set_label_position( 'bottom')
             col.set_xticks(output[4])
-            col.set_xticklabels( ('W','L','$\Gamma$','X','W','K'))
-            col.set_ylabel( 'Energy [eV]')
+            col.set_xticklabels(path_BZ)
+            #col.set_ylabel( 'Energy [eV]')
             for line in col.get_xticklines() + col.get_yticklines():
                 line.set_markersize(10)
-                line.set_markeredgewidth(2)
+                line.set_markeredgewidth(1)
             xmin = np.amin( output[0][:,0,:])
             xmax = np.amax( output[0][:,0,:])
-            col.set_xlim( [xmin, xmax])
-            col.set_ylim( [ymin, ymax])
+            col.set_xlim(xmin, xmax)
+            col.set_ylim(ymin, ymax)
 
             # band-structure and weights
             for i in range( output[1][2]):
-                col.plot( output[0][i,0,:], output[0][i,1,:], 'b', lw=3.0, zorder=10)
+                col.plot( output[0][i,0,:], output[0][i,1,:], 'b', lw=1.0, zorder=10)
             for i in range( output[1][2]):
-                col.scatter( output[0][i,0,:], output[0][i,1,:], s=(scale*output[0][i,2,:])**2, lw=3.0, edgecolor='r', facecolor='none', zorder=11)
+                normedcircles = l2norm(np.arange(0,1,1/len(output[0][i,2,:])),output[0][i,2,:])
+                #print(np.array([output[0][i,0,:]]).shape())
+                #print(np.array([output[0][i,2,:]]).shape())
+                col.scatter( output[0][i,0,:], output[0][i,1,:], s=(scale*normedcircles)**2, lw=0.35, edgecolor='r', facecolor='none', zorder=11)
 
             # Fermi level
-            col.plot( [xmin, xmax], [0, 0], 'k', lw=3.0, ls='-')
+            col.plot( [xmin, xmax], [0, 0], 'k', lw=2.0, ls='-')
 
             col2.set_ylim( col.get_ylim())
             col2.set_yticks( [0, 0])
             col2.set_yticklabels( ('$\\mathregular{E_{F}}$', ''))
 
             col.grid( True)
-            plt.title( "", fontsize=mpl.rcParams['ytick.labelsize'], y=1.03)
-            counter += 1
+            plt.title( title, fontsize=mpl.rcParams['ytick.labelsize'], y=1.03)
+            if counter >= len(files): 
+                break
+            else:
+                counter += 1
+        else: 
+            continue
+        break
+
     #save file format
     for format in output_formats:
-        fig.savefig(f"PLOT.{format}", format=format, dpi=300, bbox_inches='tight')
-    # fig.savefig(outname3, format="eps",bbox_inches='tight')
-    # fig.savefig( outname2, format='pdf', bbox_inches='tight')
-    # fig.savefig(outname4, format="svg", bbox_inches='tight')
-        # ax2 = ax1.twinx()
-
-        #  axs[] = fig.add_axes( [0.17,0.1,0.75,0.8])
-    ######################
-    # Bandstructure plot #
-    ######################
-
-    
-    # ax1.xaxis.grid( True, which='major', color='k', linestyle='-', linewidth=2)
-    # ax1.xaxis.set_label_position( 'bottom')
-   
-    # ax1.set_xticklabels( ('W','L','$\Gamma$','X','W','K'))
-    # ax1.set_ylabel( 'Energy [eV]')
-
-    # Tick size
-    
-    # xmin = np.amin( bandwgt[:,0,:])
-    # xmax = np.amax( bandwgt[:,0,:])
-    # ax1.set_xlim( [xmin, xmax])
-    # if( narg > 2): ax1.set_ylim( [ymin, ymax])
-
-    # # band-structure and weights
-    # for i in range( bandwgtdim[2]):
-    #     ax1.plot( bandwgt[i,0,:], bandwgt[i,1,:], 'b', lw=3.0, zorder=10)
-    # for i in range( bandwgtdim[2]):
-    #     ax1.scatter( bandwgt[i,0,:], bandwgt[i,1,:], s=(scale*bandwgt[i,2,:])**2, lw=3.0, edgecolor='r', facecolor='none', zorder=11)
-
-    # # Fermi level
-    # ax1.plot( [xmin, xmax], [0, 0], 'k', lw=3.0, ls='-')
-    # ax2.set_ylim( ax1.get_ylim())
-    # ax2.set_yticks( [0, 0])
-    # ax2.set_yticklabels( ('$\\mathregular{E_{F}}$', ''))
-
-    # ax1.grid( True)
-    # plt.title( "", fontsize=mpl.rcParams['ytick.labelsize'], y=1.03)
-
-    # fig.savefig( outname1, format='png', dpi=300, bbox_inches='tight')
-    # fig.savefig(outname3, format="eps",bbox_inches='tight')
-    # fig.savefig( outname2, format='pdf', bbox_inches='tight')
-    # fig.savefig(outname4, format="svg", bbox_inches='tight')
-    #plt.show()
+        if format == "png":
+            fig.savefig(f"PLOT.{format}", format="png", dpi=300, bbox_inches='tight')
+        else:
+            fig.savefig(f"PLOT.{format}",format=format, bbox_inches="tight")
     sys.exit()
+
+if __name__ == "__main__":
+    input_options = option_parser()
+    main(input_options)
